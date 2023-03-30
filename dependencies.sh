f1caa18f1bdc2bbd02885b5d93afdbf40a0be66d
xuyun030089 <64882509+yunx3arch@users.noreply.github.com>
2023-03-29 20:49:38 -0700

    change dir

--- dependencies.sh
+++ dependencies.sh
@@ -38,12 +38,16 @@ echo "auto run"
 # sudo systemctl enable webapp.service
 # sudo systemctl start webapp.service
 
-echo "install and configure cloudwatch"
+echo "install cloudwatch"
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
 sudo rpm -U ./amazon-cloudwatch-agent.rpm
 
 sudo cp cloudwatch_agent.json /opt
+ls -a 
+ls -a /opt
+ls -a /
 
+echo "configure"
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m ec2 \
