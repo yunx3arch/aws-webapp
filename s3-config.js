96e104f69c308a4acf638594b6bd1eea949f6780
xuyun030089 <64882509+yunx3arch@users.noreply.github.com>
2023-04-20 12:32:06 -0700

    ha

--- s3-config.js
+++ s3-config.js
@@ -6,13 +6,13 @@ const {
 console.log('why', process.env.AWS_ACCESS_KEY_ID);
   const s3 = new S3({
 
-      region: process.env.REGION,
+      region: "us-west-2",
       credentials: {
   
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        // accessKeyId: "AKIAVSAXDMPEGVSMPBWP",
-        // secretAccessKey: "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"
+        // accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: "AKIAVSAXDMPEGVSMPBWP",
+        secretAccessKey: "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"
   
       },
       region: "us-west-2",
