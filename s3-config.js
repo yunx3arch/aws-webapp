6d839d246b0039761021d6b202595bcbff1b423e
xuyun030089 <64882509+yunx3arch@users.noreply.github.com>
2023-04-19 18:36:01 -0700

    no more suffer

--- s3-config.js
+++ s3-config.js
@@ -8,10 +8,10 @@ const {
       // region: process.env.REGION,
       credentials: {
   
-        // accessKeyId: process.env.AWS_ACCESS_KEY,
-        // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        accessKeyId: "AKIAVSAXDMPEGVSMPBWP",
-        secretAccessKey: "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"
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        // accessKeyId: "AKIAVSAXDMPEGVSMPBWP",
+        // secretAccessKey: "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"
   
       },
       region: "us-west-2",
